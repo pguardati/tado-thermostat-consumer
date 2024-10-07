08a54bbf49f8e52c98eefc97d76d3b7021955de2
pguardati-kpler <pguardati@kpler.com>
2024-10-07 23:14:30 +0000

    chore: tmp

--- tests/test_etl.py
+++ tests/test_etl.py
@@ -2,7 +2,11 @@ import pandas as pd
 from pathlib import Path
 
 from src.common import read_json_files
-from src.stages.ingest import get_daily_intensity
+from src.stages.ingest import (
+    get_daily_intensity,
+    get_daily_targets,
+    get_daily_temperature,
+)
 from src.visualise import plot_aggregates
 
 current_file_path = Path(__name__).resolve()
@@ -12,7 +16,10 @@ staging_dir = test_dir / "staging"
 
 def _run_serial_etl(staging_dir):
     raw_row = read_json_files(staging_dir)
-    _processed = pd.concat([get_daily_intensity(data) for data in raw_row])
+
+    _intensity_raw = pd.concat([get_daily_intensity(data) for data in raw_row])
+    _targets_raw = pd.concat([get_daily_targets(data) for data in raw_row])
+    _temperature_raw = pd.concat([get_daily_temperature(data) for data in raw_row])
 
 
 def test_etl():
