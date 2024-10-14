6af7113ce202fe19649e2f3b97f688cc78ac26eb
pguardati-kpler <pguardati@kpler.com>
2024-10-14 15:27:17 +0000

    chore: tmp - clean temps

--- src/stages/aggregate_new.py
+++ src/stages/aggregate_new.py
@@ -3,15 +3,22 @@ from datetime import datetime
 import pandas as pd
 
 
-def get_master_dates(start_date, days=90):
+def get_master_dates(
+    start_date,
+    sample_time="5T",
+    days=90,
+):
     start_date = pd.to_datetime(start_date)
     end_date = start_date + pd.DateOffset(days=days)
-    dates = pd.date_range(start_date, end_date, freq="5T")
+    dates = pd.date_range(start_date, end_date, freq=sample_time)
     _dates = pd.DataFrame(dates, columns=["time"])
     return dates
 
 
-def clean_temperatures(temperatures):
+def clean_temperatures(
+    temperatures,
+    sample_time="5T",
+):
     COLUMNS_INPUT = ["time", "temperature"]
     COLUMNS_OUTPUT = ["time", "time_raw", "temperature"]
     _t = temperatures.copy()
@@ -20,7 +27,7 @@ def clean_temperatures(temperatures):
     _t["time"] = pd.to_datetime(_t["time"])
 
     _t["time_raw"] = _t["time"]
-    _t_res = _t.set_index("time").resample("30s").mean()
+    _t_res = _t.set_index("time").resample(sample_time).mean()
     _t_res = _t_res.reset_index()
     _t_res["temperature"] = _t_res["temperature"].ffill()
 
